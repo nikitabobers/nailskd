eeaf97ac2b4bb125a33d82b46505e1375819ce68
nikita <nikita.bobers@gmail.com>
2020-07-22 22:57:17 +0300

    react ts added

---
+++ client/src/components/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
+import { Footer } from "../layout/footer/Footer";
+
+function Home() {
+	return (
+		<div>
+			<Footer />
+		</div>
+	);
+}
+
+export { Home };
