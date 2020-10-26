c3b09c64dd3a5374eca87d051c3f4b831aa8cd9c
nikita <nikita.bobers@gmail.com>
2020-10-26 19:49:43 +0200

    Header layout
    React icons added

--- client/src/components/layout/header/Header.tsx
+++ client/src/components/layout/header/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navbar } from "./navbar/Navbar";
 import { HeaderTopBar } from "./topBar/HeaderTopBar";
-import "./header.css";
+import "./_header.scss";
 
 const Header = () => {
 	return (
--- client/src/components/layout/header/topBar/HeaderTopBar.tsx
+++ client/src/components/layout/header/topBar/HeaderTopBar.tsx
@@ -1,16 +1,15 @@
 import React from "react";
-import { FacebookOutlined, InstagramOutlined } from "@ant-design/icons";
+import { FiInstagram } from "react-icons/fi";
 
 const HeaderTopBar = () => {
 	return (
 		<div className="header__top">
-			<div className="header__top-language">EN</div>
 			<div className="header__top-logo">NailsKD</div>
 			<div className="header__top-social">
-				<FacebookOutlined style={{ fontSize: "18px" }} />
-				<InstagramOutlined style={{ fontSize: "18px" }} />
+				<FiInstagram />
+				<span>Instagram</span>
 			</div>
-			<div className="header__top-address">Riga</div>
+			<div className="header__top-language">EN</div>
 		</div>
 	);
 };
