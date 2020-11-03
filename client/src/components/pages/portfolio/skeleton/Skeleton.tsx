967b87e6523a282d313e3485db2670cbf2cc69ee
nikita <nikita.bobers@gmail.com>
2020-11-03 22:53:11 +0200

    Skeleton loader added

---
+++ client/src/components/pages/portfolio/skeleton/Skeleton.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ContentLoader from "react-content-loader";
+
+const Skeleton: React.FC<any> = (props) => (
+	<ContentLoader
+		width={900}
+		height={575}
+		viewBox="0 0 900 575"
+		backgroundColor="#f3f3f3"
+		foregroundColor="#ecebeb"
+		//style={{ width: "100%" }}
+		{...props}
+	>
+		<rect x="12" y="58" rx="2" ry="2" width="25%" height="33%" />
+		<rect x="240" y="57" rx="2" ry="2" width="33%" height="33%" />
+		<rect x="467" y="56" rx="2" ry="2" width="211" height="211" />
+		<rect x="12" y="283" rx="2" ry="2" width="211" height="211" />
+		<rect x="240" y="281" rx="2" ry="2" width="211" height="211" />
+		<rect x="468" y="279" rx="2" ry="2" width="211" height="211" />
+	</ContentLoader>
+);
+
+export { Skeleton };
