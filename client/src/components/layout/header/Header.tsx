68fd28658a30f082edfce2ad4b9679bf1e90d07b
nikita <nikita.bobers@gmail.com>
2020-11-26 23:13:50 +0200

    Navbar style changed

--- client/src/components/layout/header/Header.tsx
+++ client/src/components/layout/header/Header.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Navbar } from "./navbar/Navbar";
-import { HeaderTopBar } from "./topBar/HeaderTopBar";
 import "./_header.scss";
 
 const Header = () => {
 	return (
-		<div className="header">
-			<HeaderTopBar />
+		<header className="header">
 			<Navbar />
-		</div>
+		</header>
 	);
 };
 
--- client/src/components/layout/header/topBar/HeaderTopBar.tsx
+++ client/src/components/layout/header/topBar/HeaderTopBar.tsx
@@ -6,8 +6,10 @@ const HeaderTopBar = () => {
 		<div className="header__top">
 			<div className="header__top-logo">NailsKD</div>
 			<div className="header__top-social">
-				<FiInstagram />
-				<span>Instagram</span>
+				<a href="https://www.instagram.com/nailskd/">
+					<FiInstagram />
+					<span>Instagram</span>
+				</a>
 			</div>
 			<div className="header__top-language">EN</div>
 		</div>
