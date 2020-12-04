344ad63398c28d91fe04375066dd294eb81601ed
nikita <nikita.bobers@gmail.com>
2020-12-04 22:28:08 +0200

    Reservation page Confirm page created

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -7,24 +7,30 @@ import { Contact } from "./pages/contact/Contact";
 import { ReservationDate } from "./pages/reservation/dateInfo/ReservationDate";
 import { ReservationUser } from "./pages/reservation/userInfo/ReservationUser";
 import AppState from "./context/AppState";
+import { ReservationConfirm } from "./pages/reservation/confirm/ReservationConfirm";
 
 function App() {
-	return (
-		<AppState>
-			<Router>
-				<div className="container">
-					<Header />
-					<Switch>
-						<Route exact path="/" component={Home} />
-						<Route exact path="/portfolio" component={Portfolio} />
-						<Route exact path="/contact" component={Contact} />
-						<Route exact path="/reservation/date" component={ReservationDate} />
-						<Route exact path="/reservation/user" component={ReservationUser} />
-					</Switch>
-				</div>
-			</Router>
-		</AppState>
-	);
+  return (
+    <AppState>
+      <Router>
+        <div className="container">
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/reservation/date" component={ReservationDate} />
+            <Route exact path="/reservation/user" component={ReservationUser} />
+            <Route
+              exact
+              path="/reservation/confirm"
+              component={ReservationConfirm}
+            />
+          </Switch>
+        </div>
+      </Router>
+    </AppState>
+  );
 }
 
 export default App;
---
+++ client/src/components/pages/reservation/confirm/ReservationConfirm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Form } from "../../../layout/form/Form";
+
+const ReservationConfirm = () => {
+  return (
+    <section>
+      <Form>confirm page</Form>
+    </section>
+  );
+};
+
+export { ReservationConfirm };
