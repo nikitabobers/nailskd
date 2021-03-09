f97043c66b227af2f35d431bd2e106a377443869
nikita <nikita.bobers@gmail.com>
2021-03-09 20:07:01 +0200

    Server src files refactor

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,31 @@
-require("dotenv").config();
-const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
-const schema = require("./schema/root.js");
-const app = express();
-const cors = require("cors");
+require("dotenv").config({ path: "../.env" });
+import express from "express";
+import cors from "cors";
+import { graphqlHTTP } from "express-graphql";
 
+import { client } from "./config/db";
+
+const schema = require("./schema/index");
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema,
-    graphiql: true,
-  })
-);
+const main = async () => {
+  await client.connect();
 
-app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+  app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema,
+      graphiql: true,
+    })
+  );
+
+  app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
+};
+
+main().catch((err) => {
+  console.error(err);
+});
